91b70a53a4bc23685b07a3e7ebc0c6d5d3b7bee4
Francis Silva <vathansilva@gmail.com>
2025-08-04 11:26:41 +0200

    svg color change

--- src/App.vue
+++ src/App.vue
@@ -219,7 +219,7 @@
           
           <div class="action-section">
             <button class="action-button large primary" @click="submitData">
-              <svg viewBox="0 0 24 24">
+              <svg viewBox="0 0 24 24" fill="white">
                 <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
               </svg>
               Submit Configuration
