58ddd531917bdcf33ac07fe77371ebc623e69706
Francis Silva <vathansilva@gmail.com>
2025-08-04 11:31:11 +0200

    svg + icon color changed

--- src/App.vue
+++ src/App.vue
@@ -142,7 +142,7 @@
                 :disabled="!measurementMode"
                 @click="addCircleFromInputs"
               >
-                <svg viewBox="0 0 24 24">
+                <svg viewBox="0 0 24 24" fill="white">
                   <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
                 </svg>
                 Add Measurement Point
